2665b63f81a1f60279dc4cbbb3e247b8c642ebda
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-18 13:38:58 -0400

    Refactor main.tsx to use Redux Provider for store

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// redux
+import { Provider } from 'react-redux'
+import { store } from '@/store/store'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
 )
