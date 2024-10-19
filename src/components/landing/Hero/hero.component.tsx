5f4f6a63587dc0d9d982a329a692e46be4b55752
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-19 08:29:07 -0400

    Refactor Hero component to include a grid layout, heading, paragraph, and button

--- src/components/landing/Hero/hero.component.tsx
+++ src/components/landing/Hero/hero.component.tsx
@@ -1,4 +1,30 @@
+import { Button } from '@/components/ui/button'
+import { HeroCarousel } from '../'
+import { Link } from 'react-router-dom'
+
 const Hero = () => {
-  return <div>Hero</div>
+  return (
+    <section className='grid grid-cols-1 lg:grid-cols-2 gap-24 items-center'>
+      <div>
+        <h1 className='max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl'>
+          We are changing the way people shop
+        </h1>
+
+        <p className='mt-8 max-w-lg text-lg leading-8'>
+          I'm baby waistcoat gastropub slow-carb, tofu cloud bread Brooklyn
+          sartorial cornhole activated charcoal mlkshk etsy raw denim live-edge
+          cred. Microdosing tonx cred, truffaut mixtape man braid 8-bit pug
+          iPhone twee. Adaptogen praxis retro, put a bird on it brunch keffiyeh
+          mukbang man braid.
+        </p>
+
+        <Button asChild size='lg' className='mt-10'>
+          <Link to='/products'>Check Our Our Products</Link>
+        </Button>
+      </div>
+
+      <HeroCarousel />
+    </section>
+  )
 }
 export default Hero
