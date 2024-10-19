60fcddbcc9f7ea35cfde967e1f3e11f2a6b1c874
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-19 09:02:10 -0400

    Refactor products index file to fix typo in component export

--- src/components/products/index.ts
+++ src/components/products/index.ts
@@ -1,4 +1,4 @@
-export { default as Filter } from './filters.component'
+export { default as Filters } from './filters.component'
 export { default as Pagination } from './pagination.component'
 export { default as ProductsContainer } from './products-container.component'
 export { default as ProductsLust } from './products-list.component'
