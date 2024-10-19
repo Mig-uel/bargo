3e5a1fd70aae0eeb20d6567bf33ba2bb6c9c96df
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-19 08:00:46 -0400

    Refactor landing page to use react-router-dom's useLoaderData hook for fetching data

--- src/pages/Landing/landing.page.tsx
+++ src/pages/Landing/landing.page.tsx
@@ -1,6 +1,10 @@
+import { useLoaderData } from 'react-router-dom'
 import { FeaturedProducts, Hero } from '@/components/landing'
+import type { ProductsResponse } from '@/utils'
 
 const Landing = () => {
+  const { data, meta } = useLoaderData() as ProductsResponse
+
   return (
     <>
       <Hero />
