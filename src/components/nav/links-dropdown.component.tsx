4266892ea62bbeb0ae20a0f0eca4e849b6331ef3
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-19 05:11:26 -0400

    Refactor links-dropdown.component.tsx to use @radix-ui/react-dropdown-menu and add predefined links

--- src/components/nav/links-dropdown.component.tsx
+++ src/components/nav/links-dropdown.component.tsx
@@ -1,4 +1,42 @@
+import { AlignLeft } from 'lucide-react'
+import { Button } from '../ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu'
+import { links } from '@/utils'
+import { NavLink } from 'react-router-dom'
+
 const LinksDropdown = () => {
-  return <div>LinksDropdown</div>
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className='lg:hidden'>
+        <Button variant='outline' size='icon'>
+          <AlignLeft />
+          <span className='sr-only'>Toggle Links</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className='lg:hidden w-52'
+        align='start'
+        sideOffset={25}
+      >
+        {links.map((link) => (
+          <DropdownMenuItem key={link.label}>
+            <NavLink
+              to={link.href}
+              className={({ isActive }) =>
+                `capitalize w-full ${isActive ? 'text-primary' : ''}`
+              }
+            >
+              {link.label}
+            </NavLink>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
 }
 export default LinksDropdown
