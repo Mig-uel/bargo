156eb53ffbc6495cf076f24173fd186319d9b041
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-19 14:05:06 -0400

    Refactor Filters component to use FormInput for search functionality

--- src/components/products/filters.component.tsx
+++ src/components/products/filters.component.tsx
@@ -1,23 +1,16 @@
 import { Form, Link, useLoaderData } from 'react-router-dom'
-import { Label } from '../ui/label'
-import { Input } from '../ui/input'
+
 import { Button } from '../ui/button'
 import type { ProductsResponseWithParams } from '@/utils'
+import { FormInput } from '../inputs'
 
 const Filters = () => {
   const { params } = useLoaderData() as ProductsResponseWithParams
 
   return (
     <Form className='border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
-      <div className='mb-2'>
-        <Label htmlFor='search'>Search Product</Label>
-        <Input
-          id='search'
-          name='search'
-          type='text'
-          defaultValue={params?.search}
-        />
-      </div>
+      {/* search input */}
+      <FormInput name='search' type='search' defaultValue={params.search} />
 
       <Button type='submit' size='sm' className='self-end mb-2'>
         Search
