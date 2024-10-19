fddf036b461c19ebc7883d3cc02da86bd690f5ad
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-19 10:33:23 -0400

    Add Loading component export to index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from './header/header.component'
 export { default as NavBar } from './nav/navbar.component'
 export { default as ErrorElement } from './error-element/error-element.component'
+export { default as Loading } from './loading/loading.component'
