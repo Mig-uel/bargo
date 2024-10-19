cc730fc7d41f9340b26cad74423fa2e453b67274
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-19 11:26:13 -0400

    Refactor Loading component to display a grid of skeleton components for UI loading state

--- src/components/loading/loading.component.tsx
+++ src/components/loading/loading.component.tsx
@@ -1,4 +1,21 @@
+import { Skeleton } from '../ui/skeleton'
+
 const Loading = () => {
-  return <h2 className='text-xl'>Loading...</h2>
+  return (
+    <div className='pt-12 gap-4 grid md:grid-cols-2 lg:grid-cols-3'>
+      {Array.from({ length: 3 }).map((_, index) => {
+        return (
+          <div key={index} className='flex flex-col space-y-3'>
+            <Skeleton className='h-[125px] w-full rounded-xl' />
+
+            <div className='space-y-2'>
+              <Skeleton className='h-4 mx-auto w-[250px]' />
+              <Skeleton className='h-4 mx-auto w-[250px]' />
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 export default Loading
