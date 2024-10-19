d3b699f7230124a7674df85796e3fcd766fdb236
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-19 11:55:33 -0400

    Refactor products loader to include request parameters in API call

--- src/loaders/products/loader.ts
+++ src/loaders/products/loader.ts
@@ -3,8 +3,18 @@ import type { LoaderFunction } from 'react-router-dom'
 
 const url = '/products'
 
-const loader: LoaderFunction = async (): Promise<ProductsResponse> => {
-  const res = await customFetch<ProductsResponse>(url)
+const loader: LoaderFunction = async ({
+  request,
+}): Promise<ProductsResponse> => {
+  // create url from request using WebAPI URL
+  const newURL = new URL(request.url)
+
+  // create params object from `newURL` searchParams entries
+  const params = Object.fromEntries([...newURL.searchParams.entries()])
+
+  const res = await customFetch<ProductsResponse>(url, {
+    params,
+  })
 
   return {
     ...res.data,
