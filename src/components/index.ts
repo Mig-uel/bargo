51b7775e7264bed6a53881f44944e79884c3122d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-19 06:19:25 -0400

    Add NavBar and ErrorElement components to the component index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
 export { default as Header } from './header/header.component'
+export { default as NavBar } from './nav/navbar.component'
+export { default as ErrorElement } from './error-element/error-element.component'
