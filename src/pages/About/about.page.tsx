6cd067f53b2caa4f2f04ffcd11f4a4cf28f3cbab
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-19 06:14:16 -0400

    Refactor About page to add a section with a heading and paragraph

--- src/pages/About/about.page.tsx
+++ src/pages/About/about.page.tsx
@@ -1,4 +1,17 @@
 const About = () => {
-  return <div>About</div>
+  return (
+    <section>
+      <h1 className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-wide sm:text-6xl'>
+        We ❤️ Bargains!
+      </h1>
+
+      <p className='mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto'>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum ex
+        ipsa sit eaque assumenda velit ullam! Iste veritatis ea vel eum,
+        aspernatur consequuntur facilis quisquam voluptate perferendis, placeat,
+        accusamus dolor!
+      </p>
+    </section>
+  )
 }
 export default About
