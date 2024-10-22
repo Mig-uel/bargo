e1754fc2439ad06f8a2d6fc00eccd2003ebb4010
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-22 12:19:40 -0400

    Add FormCheckbox export to index.ts

--- src/components/inputs/index.ts
+++ src/components/inputs/index.ts
@@ -1,3 +1,4 @@
 export { default as FormInput } from './form-input.component'
 export { default as FormSelect } from './form-select.component'
 export { default as FormRange } from './form-range.component'
+export { default as FormCheckbox } from './form-checkbox.component'
