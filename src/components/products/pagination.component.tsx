c4cad3f942e8db1fc116df0dfcce48314de28ab8
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-22 13:27:47 -0400

    Refactor Pagination component to use react-router-dom for navigation and add pagination utility functions for constructing URLs

--- src/components/products/pagination.component.tsx
+++ src/components/products/pagination.component.tsx
@@ -1,4 +1,67 @@
+import { useLoaderData, useLocation } from 'react-router-dom'
+import {
+  Pagination as PaginationUI,
+  PaginationItem,
+  PaginationLink,
+  PaginationContent,
+  PaginationPrevious,
+  PaginationNext,
+} from '../ui/pagination'
+
+import {
+  constructPrevOrNextURL,
+  constructURL,
+  type ProductsResponseWithParams,
+} from '@/utils'
+
 const Pagination = () => {
-  return <div>Pagination</div>
+  const { meta } = useLoaderData() as ProductsResponseWithParams
+
+  const {
+    pagination: { page, pageCount },
+  } = meta
+
+  const { search, pathname } = useLocation()
+
+  const pages = Array.from({ length: pageCount }, (_, index) => index + 1)
+
+  if (pageCount < 2) return null
+
+  const renderPagination = pages.map((pageNumber) => {
+    const isActive = pageNumber === page
+
+    // construct url for all the pageNumbers in the array.
+    const url = constructURL({ pageNumber, search, pathname })
+
+    // for each pageNumber in the array, we return a pagination item with its constructed url
+    return (
+      <PaginationItem key={pageNumber}>
+        <PaginationLink to={url} isActive={isActive}>
+          {pageNumber}
+        </PaginationLink>
+      </PaginationItem>
+    )
+  })
+
+  const { prevURL, nextURL } = constructPrevOrNextURL({
+    currentPage: page,
+    pageCount,
+    pathname,
+    search,
+  })
+
+  return (
+    <PaginationUI className='mt-16'>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious to={prevURL} />
+        </PaginationItem>
+        {renderPagination}
+        <PaginationItem>
+          <PaginationNext to={nextURL} />
+        </PaginationItem>
+      </PaginationContent>
+    </PaginationUI>
+  )
 }
 export default Pagination
