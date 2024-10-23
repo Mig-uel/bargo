3394df0bb67d73364f70dfa3469f597fee723b72
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-23 08:43:15 -0400

    Refactor cart component exports to use consistent naming convention

--- src/components/cart/index.ts
+++ src/components/cart/index.ts
@@ -1,4 +1,4 @@
 export { default as CartButtons } from './cart-buttons.component'
-export { default as CartItemCol } from './cart-item-cols.component'
-export { default as CartItemsList } from './cart-items-list.component'
 export * from './cart-item-cols.component'
+export { default as CartItemsList } from './cart-items-list.component'
+export { default as CartTotals } from './cart-totals.component'
