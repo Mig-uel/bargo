4b460507e80cd7a84b3d29514d10ff6d1ee111ca
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-23 15:06:13 -0400

    Refactor cart index.ts to remove export of CartButtons component

--- src/components/cart/index.ts
+++ src/components/cart/index.ts
@@ -1,4 +1,3 @@
-export { default as CartButtons } from './cart-buttons.component'
 export * from './cart-item-cols.component'
 export { default as CartItemsList } from './cart-items-list.component'
 export { default as CartTotals } from './cart-totals.component'
