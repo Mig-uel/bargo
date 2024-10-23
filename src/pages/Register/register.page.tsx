221ff47d6c7f3192f585ee72446a36b7cc856712
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-23 15:41:38 -0400

    Refactor Register page to include registration form and submit button

--- src/pages/Register/register.page.tsx
+++ src/pages/Register/register.page.tsx
@@ -1,4 +1,54 @@
+import { registerAction } from '@/actions'
+import { FormInput } from '@/components/inputs'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Form, Link } from 'react-router-dom'
+
 const Register = () => {
-  return <div>Register</div>
+  return (
+    <section className='h-screen grid place-items-center'>
+      <Card className='w-96 bg-muted'>
+        <CardHeader>
+          <CardTitle className='text-center capitalize'>register</CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          <Form>
+            <FormInput
+              name='username'
+              label='username'
+              type='text'
+              defaultValue='test'
+            />
+
+            <FormInput
+              name='email'
+              type='email'
+              defaultValue='test@test.com'
+              label='email'
+            />
+
+            <FormInput
+              name='password'
+              type='password'
+              defaultValue='secret'
+              label='password'
+            />
+
+            <Button type='submit' variant='default' className='mt-4 w-full'>
+              Register
+            </Button>
+
+            <p className='text-center mt-4'>
+              Already a member?
+              <Button type='button' asChild variant='link'>
+                <Link to='/login'>Login</Link>
+              </Button>
+            </p>
+          </Form>
+        </CardContent>
+      </Card>
+    </section>
+  )
 }
 export default Register
