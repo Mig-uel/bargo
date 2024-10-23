785e26c42a0d4e6a010af7a96a4aa44d1e490052
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-23 15:31:42 -0400

    Add login and register actions to router configuration

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import {
 } from './pages'
 import { ErrorElement } from './components'
 import { landingLoader, productsLoader, singleProductLoader } from './loaders'
+import { loginAction, registerAction } from './actions'
 
 const router = createBrowserRouter([
   {
@@ -66,11 +67,13 @@ const router = createBrowserRouter([
     path: '/login',
     element: <Login />,
     errorElement: <Error />,
+    action: loginAction,
   },
   {
     path: '/register',
     element: <Register />,
     errorElement: <Error />,
+    action: registerAction,
   },
 ])
 
