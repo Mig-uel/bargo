6b623e0ce4a83e04f1a46b408b2eeddb838e8ad9
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-23 08:24:38 -0400

    Refactor Cart page to display cart items, totals, and checkout options

--- src/pages/Cart/cart.page.tsx
+++ src/pages/Cart/cart.page.tsx
@@ -1,4 +1,42 @@
+import { CartItemsList, CartTotals } from '@/components/cart'
+import { SectionTitle } from '@/components/landing'
+import { Button } from '@/components/ui/button'
+import { useAppSelector } from '@/store/hooks'
+import { Link } from 'react-router-dom'
+
 const Cart = () => {
-  return <div>Cart</div>
+  // temp
+  const user = null
+
+  const numItemsInCart = useAppSelector((state) => state.cart.numItemsInCart)
+
+  if (!numItemsInCart) return <SectionTitle text='Empty cart' />
+
+  return (
+    <>
+      <SectionTitle text='Shopping Cart' />
+
+      <div className='mt-8 grid gap-8 lg:grid-cols-12'>
+        {/* CART ITEMS LIST */}
+        <div className='lg:col-span-8'>
+          <CartItemsList />
+        </div>
+
+        {/* CART TOTALS */}
+        <div className='lg:col-span-4 lg:pl-4'>
+          <CartTotals />
+          {user ? (
+            <Button asChild className='mt-8 w-full'>
+              <Link to='/checkout'>Checkout</Link>
+            </Button>
+          ) : (
+            <Button asChild className='mt-8 w-full'>
+              <Link to='/login'>Login to Checkout</Link>
+            </Button>
+          )}
+        </div>
+      </div>
+    </>
+  )
 }
 export default Cart
