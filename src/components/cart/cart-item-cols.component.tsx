999035d9aa31424df09d9279c9e21f8f873a291e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-23 14:36:51 -0400

    Refactor cart item component's second column to display company, product title, and color

--- src/components/cart/cart-item-cols.component.tsx
+++ src/components/cart/cart-item-cols.component.tsx
@@ -14,8 +14,33 @@ export const FirstCol = ({
   )
 }
 
-export const SecondCol = () => {
-  return <h4>Second Col</h4>
+export const SecondCol = ({
+  company,
+  productColor,
+  title,
+}: {
+  company: string
+  productColor: string
+  title: string
+}) => {
+  return (
+    <div className='sm:ml-4 md:ml-12 sm:w-48'>
+      <h3 className='capitalize font-medium'>{title}</h3>
+      <h4 className='mt-2 capitalize text-sm'>{company}</h4>
+
+      <p className='mt-4 text-sm capitalize flex items-center gap-x-2'>
+        color:{' '}
+        <span
+          style={{
+            width: '15px',
+            height: '15px',
+            borderRadius: '50%',
+            backgroundColor: productColor,
+          }}
+        ></span>
+      </p>
+    </div>
+  )
 }
 
 export const ThirdCol = () => {
