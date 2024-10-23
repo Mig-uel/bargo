1107031245c109feec559bbb4675cadc1ccecb4c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-23 16:20:25 -0400

    Refactor Register action to handle registration form submission and display appropriate toast messages

---
+++ src/actions/register/action.ts
@@ -0,0 +1,33 @@
+import { redirect, type ActionFunction } from 'react-router-dom'
+import { customFetch } from '@/utils'
+import { toast } from '@/hooks/use-toast'
+import { AxiosError } from 'axios'
+
+const action: ActionFunction = async ({
+  request,
+}): Promise<Response | null> => {
+  const formData = await request.formData()
+  const userData = Object.fromEntries(formData)
+
+  try {
+    const res = await customFetch.post('/auth/local/register', userData)
+
+    console.log(res)
+
+    toast({ description: 'Successfully registered!' })
+
+    return redirect('/login')
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      toast({
+        description: error.response?.data.error.message,
+      })
+    } else {
+      toast({ description: 'Registration Failed' })
+    }
+
+    return null
+  }
+}
+
+export default action
