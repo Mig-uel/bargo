7afa61e876fc5bf6bb977c27f1799f0eb08dab85
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-25 12:22:48 -0400

    Refactor Orders page to include loader data and pagination

--- src/pages/Orders/orders.page.tsx
+++ src/pages/Orders/orders.page.tsx
@@ -1,11 +1,32 @@
+import { ComplexPagination } from '@/components'
+import { SectionTitle } from '@/components/landing'
+import OrdersList from '@/components/orders/orders-list.component'
+import { Button } from '@/components/ui/button'
 import { OrdersResponse } from '@/utils'
-import { useLoaderData } from 'react-router-dom'
+import { Link, useLoaderData } from 'react-router-dom'
 
 const Orders = () => {
-  const { meta, data: orders } = useLoaderData() as OrdersResponse
+  const { meta } = useLoaderData() as OrdersResponse
 
-  console.log(meta)
+  if (meta.pagination.total < 1)
+    return (
+      <>
+        <SectionTitle text='No Orders' />
 
-  return <div>Orders</div>
+        <Button asChild className='mt-8 w-full md:w-40'>
+          <Link to='/products'>Go to Products</Link>
+        </Button>
+      </>
+    )
+
+  return (
+    <>
+      <SectionTitle text='Your Orders' />
+
+      <OrdersList />
+
+      <ComplexPagination />
+    </>
+  )
 }
 export default Orders
