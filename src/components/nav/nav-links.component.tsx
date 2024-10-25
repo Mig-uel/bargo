522a0135edea2574f5de14eef17654ff68a21310
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-25 10:07:34 -0400

    Refactor NavLinks component to conditionally render 'orders' link based on user authentication status

--- src/components/nav/nav-links.component.tsx
+++ src/components/nav/nav-links.component.tsx
@@ -8,7 +8,9 @@ const NavLinks = () => {
   return (
     <div className='hidden lg:flex justify-center items-center gap-x-4'>
       {links.map((link) => {
-        if (link.label === 'orders' && !user) return
+        const restrictedLink = link.label === 'orders'
+
+        if (restrictedLink && !user) return
 
         return (
           <NavLink
