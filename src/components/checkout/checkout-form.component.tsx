7fa6db716fa6ce3793c903fd7064583b9d94cd00
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-25 11:39:42 -0400

    Refactor CheckoutForm component to include a shipping information form

--- src/components/checkout/checkout-form.component.tsx
+++ src/components/checkout/checkout-form.component.tsx
@@ -1,4 +1,17 @@
+import { Form } from 'react-router-dom'
+import { FormInput } from '../inputs'
+import { SubmitButton } from '../register'
+
 const CheckoutForm = () => {
-  return <div>CheckoutForm</div>
+  return (
+    <Form method='post' className='flex flex-col gap-y-4'>
+      <h1 className='font-medium text-lg mb-4'>Shipping Info</h1>
+
+      <FormInput label='name' type='text' name='name' />
+      <FormInput label='address' type='text' name='address' />
+
+      <SubmitButton text='place order' className='capitalize mt-4' />
+    </Form>
+  )
 }
 export default CheckoutForm
