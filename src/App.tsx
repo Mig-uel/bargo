3641cbf30daa5b14cf7c9fca11dfffa85cec56f7
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-25 11:37:43 -0400

    Refactor App.tsx to update checkoutLoader usage and accept store as argument

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ const router = createBrowserRouter([
         path: 'checkout',
         element: <Checkout />,
         errorElement: <ErrorElement />,
-        action: checkoutAction,
+        action: checkoutAction(store),
       },
       {
         path: 'orders',
