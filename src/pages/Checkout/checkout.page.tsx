1646ac268d95e21a60b47d1cb64e55f8e30b7909
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-25 11:53:28 -0400

    Refactor Checkout page to include user authentication check and display cart information

--- src/pages/Checkout/checkout.page.tsx
+++ src/pages/Checkout/checkout.page.tsx
@@ -1,4 +1,44 @@
+import { useEffect } from 'react'
+
+import { SectionTitle } from '@/components/landing'
+import { Button } from '@/components/ui/button'
+import { useAppSelector } from '@/store/hooks'
+import { Link, useNavigate } from 'react-router-dom'
+import { CheckoutForm } from '@/components/checkout'
+import { CartTotals } from '@/components/cart'
+
 const Checkout = () => {
-  return <div>Checkout</div>
+  const navigate = useNavigate()
+  const user = useAppSelector((state) => state.user.user)
+  const cartTotal = useAppSelector((state) => state.cart.cartTotal)
+
+  useEffect(() => {
+    if (!user)
+      return navigate('/login', {
+        replace: true,
+      })
+  }, [navigate, user])
+
+  if (!cartTotal)
+    return (
+      <>
+        <SectionTitle text='Cart is Empty' />
+
+        <Button asChild className='mt-8 w-full md:w-40'>
+          <Link to='/products'>Go to Products</Link>
+        </Button>
+      </>
+    )
+
+  return (
+    <>
+      <SectionTitle text='Checkout' />
+
+      <div className='mt-8 grid gap-8 md:grid-cols-2 items-start'>
+        <CheckoutForm />
+        <CartTotals />
+      </div>
+    </>
+  )
 }
 export default Checkout
