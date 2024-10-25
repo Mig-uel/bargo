b761a261bbeb5afb13791e2f087cc773fc76be74
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-25 12:08:54 -0400

    Refactor Orders page to redirect to login if user is not authenticated

--- src/pages/Orders/orders.page.tsx
+++ src/pages/Orders/orders.page.tsx
@@ -1,4 +1,15 @@
+import { useAppSelector } from '@/store/hooks'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 const Orders = () => {
+  const navigate = useNavigate()
+  const user = useAppSelector((state) => state.user.user)
+
+  useEffect(() => {
+    if (!user) return navigate('/login', { replace: true })
+  }, [navigate, user])
+
   return <div>Orders</div>
 }
 export default Orders
