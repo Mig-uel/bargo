083228b35cb4e9a9867a2bcb7141cf4fe57076c6
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-25 10:06:49 -0400

    Refactor NavLinks component to conditionally render 'orders' link based on user authentication status

--- src/components/nav/nav-links.component.tsx
+++ src/components/nav/nav-links.component.tsx
@@ -1,22 +1,29 @@
+import { useAppSelector } from '@/store/hooks'
 import { links } from '@/utils'
 import { NavLink } from 'react-router-dom'
 
 const NavLinks = () => {
+  const user = useAppSelector((state) => state.user.user)
+
   return (
     <div className='hidden lg:flex justify-center items-center gap-x-4'>
-      {links.map((link) => (
-        <NavLink
-          to={link.href}
-          key={link.label}
-          className={({ isActive }) =>
-            `capitalize font-light tracking-wide ${
-              isActive ? 'text-primary' : ''
-            }`
-          }
-        >
-          {link.label}
-        </NavLink>
-      ))}
+      {links.map((link) => {
+        if (link.label === 'orders' && !user) return
+
+        return (
+          <NavLink
+            to={link.href}
+            key={link.label}
+            className={({ isActive }) =>
+              `capitalize font-light tracking-wide ${
+                isActive ? 'text-primary' : ''
+              }`
+            }
+          >
+            {link.label}
+          </NavLink>
+        )
+      })}
     </div>
   )
 }
