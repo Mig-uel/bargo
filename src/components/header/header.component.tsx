80bbedc70bc9e3351280247234e8e3a70a2f31aa
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-25 10:01:12 -0400

    Refactor Header component to use redux store for user data and add logout functionality

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { useAppDispatch, useAppSelector } from '@/store/hooks'
 import { Button } from '../ui/button'
+import { useToast } from '@/hooks/use-toast'
+import { clearCart } from '@/store/cart/cartSlice'
+import { logout } from '@/store/user/userSlice'
 
 const Header = () => {
+  const user = useAppSelector((state) => state.user.user)
+  const dispatch = useAppDispatch()
+  const { toast } = useToast()
   const navigate = useNavigate()
-  const [user, setUser] = useState<{ username: string } | null>({
-    username: 'Guest',
-  })
+
   const handleLogout = () => {
-    setUser(null)
+    dispatch(clearCart())
+    dispatch(logout())
+    toast({ description: 'Logged out' })
     navigate('/')
   }
 
@@ -19,7 +25,9 @@ const Header = () => {
 
         {user ? (
           <div className='flex gap-x-2 sm:gap-x-8 items-center'>
-            <p className='text-xs sm:text-sm'>Hello, {user.username}!</p>
+            <p className='text-xs sm:text-sm capitalize'>
+              Hello, {user.username}!
+            </p>
             <Button variant='link' size='sm' onClick={handleLogout}>
               Logout
             </Button>
