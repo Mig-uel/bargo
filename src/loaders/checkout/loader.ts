5afea82d05d8f05049457d58c050e4de442f9d24
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-25 11:35:40 -0400

    Remove checkoutLoader from loaders/index.ts

--- src/loaders/checkout/loader.ts
+++
@@ -1,5 +0,0 @@
-import { LoaderFunction } from 'react-router-dom'
-
-const loader: LoaderFunction = async () => {}
-
-export default loader
--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,4 +1,3 @@
 export { default as landingLoader } from './landing/loader'
 export { default as productsLoader } from './products/loader'
 export { default as singleProductLoader } from './single-product/loader'
-export { default as checkoutLoader } from './checkout/loader'
